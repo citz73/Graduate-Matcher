f891535a92320919943a144932398522c45b557a
Israel Miles <ismi7509@colorado.edu>
2019-11-13 18:16:31 -0700

    building views for deadlines

--- gradmatch/gradmatchapp/views.py
+++ gradmatch/gradmatchapp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Deadline
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is the gradmatchapp homepage")
+    deadline_list = Deadline.objects.all()
+    output = ','.join(deadline.name for deadline in deadline_list)
+    return HttpResponse(output)
 
 
 def detail(request):
