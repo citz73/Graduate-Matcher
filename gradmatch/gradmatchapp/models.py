0120f0285f0c64755b296154e374fa7ebc4c779a
Israel Miles <ismi7509@colorado.edu>
2019-11-13 17:53:44 -0700

    add data to model

--- gradmatch/gradmatchapp/models.py
+++ gradmatch/gradmatchapp/models.py
@@ -4,5 +4,10 @@ from django.db import models
 
 
 class Deadline(models.Model):
+
+    def __str__(self):
+        return self.name
+
+        
     name = models.CharField(max_length=200)
     priority = models.CharField(max_length=200)
