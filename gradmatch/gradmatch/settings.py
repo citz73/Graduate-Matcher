1274ad0b97b2f61b30f3fc4a481e2a420f018ac7
Israel Miles <ismi7509@colorado.edu>
2019-11-13 17:41:20 -0700

    create model for deadlines

--- gradmatch/gradmatch/settings.py
+++ gradmatch/gradmatch/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gradmatchapp.apps.GradmatchappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
