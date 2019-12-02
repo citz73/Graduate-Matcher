33de5ebb6ce68988a535d1674c447c732d172c15
Israel Miles <ismi7509@colorado.edu>
2019-12-01 19:12:41 -0700

    remove hardcoded urls

--- gradmatch/gradmatchapp/urls.py
+++ gradmatch/gradmatchapp/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from gradmatchapp import views
 
+
+app_name = 'gradmatchapp'
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:deadline_id>/', views.detail, name='detail'),
