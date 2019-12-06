a3284e64cf60f81721130578894d9e478fbd4afc
Israel Miles <ismi7509@colorado.edu>
2019-12-06 16:55:24 -0700

    update

--- gradmatch/urls.py
+++ gradmatch/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('gradmatchapp/', include('gradmatchapp.urls')),
-    path('/', include('gradmatchapp.urls')),
+    path('', include('gradmatchapp.urls')),
 ]
